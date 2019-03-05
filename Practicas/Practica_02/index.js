ea5ed39bdb755cdd860dc71218815718510061a3
Kwazema <andreu.kwazema@gmail.com>
2019-03-06 00:18:38 +0100

    Added Starts in Films

--- Practicas/Practica_02/index.js
+++ Practicas/Practica_02/index.js
@@ -60,7 +60,7 @@ const onSubmit = () => {
 
 
 //const showFilmInfo = (name, capital, flagURL) => {
-const showFilmInfo = (title, imgURL, desc) => {
+const showFilmInfo = (title, imgURL, desc, average) => {
     
     //----------- RENDER TITLE -----------//
     const titleElement = document.createElement('h2');
@@ -87,36 +87,63 @@ const showFilmInfo = (title, imgURL, desc) => {
     descElement.appendChild(descText);
 
     
+
+    //-------------- AVERAGE -------------//
+    const getAverage = [ 2, 4, 6, 8, 10 ];
+    let numStarts = 0;
+    let findAverage = false; 
+
+    for (let i = 0; i < getAverage.length && !findAverage; i++) {
+        if (average <= getAverage[i])
+        {
+            numStarts = i + 1;
+            findAverage = true;
+        }
+    }
+
     //---------- RENDER START -----------//
-    const startElement = document.createElement('img');
-    startElement.src = "start.png";
+    const startElement = new Array(numStarts);
+    
+    for (i = 0; i < numStarts; i++) {
+        startElement[i] = document.createElement('img');
 
-    // contentDiv.appendChild(startElement);
-    startElement.height = 50;
-    startElement.width = 50;
-
+        startElement[i].src = "start.png";
+        startElement[i].src = "start.png";
+    
+        startElement[i].height = 50;
+        startElement[i].width = 50;
+    }
 
     //-------- RENDER CONTENT FILM -------//
+    const infoElement = document.createElement('div');
+    const startsElement = document.createElement('div');
     const filmElement = document.createElement('div');
 
+    //----------------------------------//
+
+    infoElement.classList.add('film_info');
+    startsElement.classList.add('film_starts');
     filmElement.classList.add('film');
-    filmElement.appendChild(titleElement);
-    filmElement.appendChild(imgElement);
-    filmElement.appendChild(descElement);
+    
+    //----------------------------------//
+
+    infoElement.appendChild(titleElement);
+    infoElement.appendChild(imgElement);
+    infoElement.appendChild(descElement);
+    
+    startElement.forEach(element => {
+        startsElement.appendChild(element);
+    }); 
+
+    //----------------------------------//
+
+    filmElement.appendChild(infoElement);
+    filmElement.appendChild(startsElement);
 
     contentDiv.appendChild(filmElement);
-
-
-    //------- RENDER CONTENT START ------//
-    const startsElement = document.createElement('div');
-
-    startsElement.classList.add('start');
-    startsElement.appendChild(startElement);
-
-    filmElement.appendChild(startsElement);
 }
-
-onRequestSuccess = json => {
+    
+const onRequestSuccess = (json) => {
     console.log('Request', json.results);
     const films = json.results;
 
@@ -126,7 +153,7 @@ onRequestSuccess = json => {
 
     if (films !== null) {    
         films.forEach(film => {
-            showFilmInfo(film.title, film.poster_path, film.overview);
+            showFilmInfo(film.title, film.poster_path, film.overview, film.vote_average);
         });     
         
         lengthChildDiv = contentDiv.childNodes.length
@@ -158,5 +185,3 @@ const deleteChilds = () => {
     }
     drop.style.display='none';
 }
-
-  
